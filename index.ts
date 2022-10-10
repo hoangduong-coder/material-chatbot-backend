a7a89a699cbdf268d6341b9615fd39ae6b5f855e
Hoang Duong <hoangduongphantri@gmail.com>
2022-10-10 19:11:44 +0300

    implement further packages

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+// import cors from "cors";
+
 import dotenv from "dotenv";
 import express from "express";
 import qnaRouter from "./src/router/questionAnswering";
@@ -6,9 +8,21 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 
-const port = process.env["SERVER_PORT"];
+const port = process.env["LOCAL_SERVER_PORT"];
+// const endpoint = process.env["LANGUAGE_STUDIO_ENDPOINT"];
 
-app.use("/api/questionAndAnswer", qnaRouter);
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:3000",
+//     `http://${endpoint}.api.cognitive.microsoft.com//language/:query-knowledgebases?projectName=material-chatbot&api-version=2021-10-01&deploymentName=test`,
+//   ],
+//   allowedHeaders: ["Ocp-Apim-Subscription-Key", "Content-Type"],
+// };
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+// app.use(cors(corsOptions));
+
+app.use("/", qnaRouter);
 
 app.listen(port, () => {
   console.log(`Hello, this is port ${port}`);

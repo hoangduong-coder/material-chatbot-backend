dea0a460e8968eb95a309e1d4dc439df8a53dddb
Hoang Duong <hoangduongphantri@gmail.com>
2022-10-11 17:27:05 +0300

    Uncommented imports and variables

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-// import cors from "cors";
-
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import qnaRouter from "./src/router/questionAnswering";
@@ -9,18 +8,18 @@ app.use(express.json());
 dotenv.config();
 
 const port = process.env["LOCAL_SERVER_PORT"];
-// const endpoint = process.env["LANGUAGE_STUDIO_ENDPOINT"];
+const endpoint = process.env["LANGUAGE_STUDIO_ENDPOINT"];
 
-// const corsOptions = {
-//   origin: [
-//     "http://localhost:3000",
-//     `http://${endpoint}.api.cognitive.microsoft.com//language/:query-knowledgebases?projectName=material-chatbot&api-version=2021-10-01&deploymentName=test`,
-//   ],
-//   allowedHeaders: ["Ocp-Apim-Subscription-Key", "Content-Type"],
-// };
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    `http://${endpoint}.api.cognitive.microsoft.com//language/:query-knowledgebases?projectName=material-chatbot&api-version=2021-10-01&deploymentName=test`,
+  ],
+  allowedHeaders: ["Ocp-Apim-Subscription-Key", "Content-Type"],
+};
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use("/", qnaRouter);
 

2161d0891381464e2e0681c021f84fb443c832cf
Hoang Duong <hoangduongphantri@gmail.com>
2022-11-29 15:11:35 +0200

    Remove unused packages

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import materialRouter from "./src/router/material";
@@ -9,14 +8,6 @@ app.use(express.json());
 dotenv.config();
 
 const port = process.env["LOCAL_SERVER_PORT"];
-
-const corsOptions = {
-  origin: ["http://localhost:3000"],
-  allowedHeaders: ["Ocp-Apim-Subscription-Key", "Content-Type"],
-};
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-app.use(cors(corsOptions));
 
 app.use("/qna", qnaRouter);
 

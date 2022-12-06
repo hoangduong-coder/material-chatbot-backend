bb5ba17b8b56f5dc410a3c8fcad451f7ff5547c2
Hoang Duong <63698826+hoangduong-coder@users.noreply.github.com>
2022-12-06 16:56:27 +0200

    Disable warnings

--- src/service/question-and-answer/rangeQuestion.ts
+++ src/service/question-and-answer/rangeQuestion.ts
@@ -1,31 +1,35 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 import List from "../../data/material.json";
 import { Entity } from "../../types/helperTypes/clu";
 
 const RangeQuestion = (props: Array<Entity>) => {
-  let ans = ''
-  let check: string = ''
-  const value: Entity | undefined = props.find(en => en.category === 'Value')
-  const searchKey = props.find(en => en.category === 'Search Key')?.extraInformation?.[0].key!
-  if (!value || ! searchKey) return ans = 'No answer found!'
+  let ans = "";
+  let check = "";
+  const value: Entity | undefined = props.find((en) => en.category === "Value");
+  const searchKey = props.find((en) => en.category === "Search Key")
+    ?.extraInformation?.[0].key!;
+  if (!value || !searchKey) return ("No answer found!");
 
-  const max = value.resolutions?.[0].maximum!
-  const min = value.resolutions?.[0].minimum!
+  const max = value.resolutions?.[0].maximum!;
+  const min = value.resolutions?.[0].minimum!;
 
-  if (typeof max === 'string') check = 'bigger'
-  else if (typeof min === 'string') check = 'smaller'
+  if (typeof max === "string") check = "bigger";
+  else if (typeof min === "string") check = "smaller";
 
-  const listSearched = List.filter(ls => {
+  const listSearched = List.filter((ls) => {
     switch (check) {
-      case 'bigger':
+      case "bigger":
         return ls[searchKey] >= min;
-      case 'smaller':
-        return ls[searchKey] <= max
+      case "smaller":
+        return ls[searchKey] <= max;
       default:
         return ls[searchKey] >= min && ls[searchKey] <= max;
-      }
-  })
-  ans = listSearched.map((s) => s["Material ID"]).join(', ');
-  return ans
+    }
+  });
+  ans = listSearched.map((s) => s["Material ID"]).join(", ");
+  return ans;
 }
 
 export default RangeQuestion;

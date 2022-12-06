b1eaee75347debefa967accbc7760c2f2f1f0113
Hoang Duong <63698826+hoangduong-coder@users.noreply.github.com>
2022-12-06 16:58:28 +0200

    Disable warnings and change type assertion

--- src/service/question-and-answer/selection.ts
+++ src/service/question-and-answer/selection.ts
@@ -1,19 +1,24 @@
-import List from "../../data/material.json";
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 import { Entity } from "../../types/helperTypes/clu";
+import List from "../../data/material.json";
 import { Material } from "../../types/helperTypes/material";
 
 const Selection = (props: Array<Entity>) => {
-  let ans = ''
-  props.forEach(c => {
-    const searched: Material | undefined = List.find(l => l[c.category].toUpperCase() === c.text.toUpperCase())
-    if (!searched) return ans = "No answer found!";
-    ans += `\u{2605}Information of ${c.text!}\n`
+  let ans = "";
+  props.forEach((c) => {
+    const searched: Material | undefined = List.find(
+      (l) => l[c.category].toUpperCase() === c.text.toUpperCase()
+    );
+    if (!searched)
+      return "No answer found!";
+    ans += `\u{2605}Information of ${c.text}\n`;
     for (const [key, value] of Object.entries(searched)) {
       ans += `\u{2606} ${key}: ${value}\n`;
     }
-    return ans
-  })
-  return ans
-}
+    return ans;
+  });
+  return ans;
+};
 
 export default Selection;

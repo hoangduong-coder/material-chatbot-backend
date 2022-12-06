6bc8b33499bc90ae21eba79ac96c381e3e7f73e4
Hoang Duong <63698826+hoangduong-coder@users.noreply.github.com>
2022-12-06 16:51:30 +0200

    Change variables declarations

--- src/service/question-and-answer/equivalentQuestion.ts
+++ src/service/question-and-answer/equivalentQuestion.ts
@@ -3,17 +3,26 @@ import { Entity } from "../../types/helperTypes/clu";
 import { Material } from "../../types/helperTypes/material";
 
 const EquivalentQuestion = (props: Array<Entity>) => {
-  let ans = ''
-  const listCode: Array<Entity> = []
-  props.forEach(sk => {if(sk.category === "Material ID" || sk.category === "International Standard") listCode.push(sk)})
-  if (!listCode) return ans = "No answer found!";
-  listCode.forEach(lc => {
-    const searched: Material | undefined = List.find(l => l[lc.category].replace(/ /g,'') === lc.text.toUpperCase().replace(/ /g,''))
-    if (!searched) return ans = "No answer found!";
-    const listSearched = List.filter(ls => ls.Remarks === searched.Remarks)
-    return ans += `The material similar to ${lc.text}: ${listSearched.map((s) => s['Material ID']).join(', ')}\n`
-  })
-  return ans
+  let ans = "";
+  const listCode: Array<Entity> = props.filter(
+    (sk) =>
+      sk.category === "Material ID" || sk.category === "International Standard"
+  );
+  if (!listCode) return "No answer found!";
+  listCode.forEach((lc) => {
+    const searched: Material | undefined = List.find(
+      (l) =>
+        l[lc.category].replace(/ /g, "") ===
+        lc.text.toUpperCase().replace(/ /g, "")
+    );
+    if (!searched) return "No answer found!";
+    const listSearched = List.filter((ls) => ls.Remarks === searched.Remarks);
+    ans += `The material similar to ${lc.text}: ${listSearched
+      .map((s) => s["Material ID"])
+      .join(", ")}\n`;
+    return ans;
+  });
+  return ans;
 }
 
 export default EquivalentQuestion;

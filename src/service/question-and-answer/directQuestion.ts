f4eec7f84d00b56bf7de0aa796d417ba5b4df07d
Hoang Duong <63698826+hoangduong-coder@users.noreply.github.com>
2022-12-06 16:16:30 +0200

    Fix directQuestion variables declaration

--- src/service/question-and-answer/directQuestion.ts
+++ src/service/question-and-answer/directQuestion.ts
@@ -1,24 +1,37 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 import List from "../../data/material.json";
 import { Entity } from "../../types/helperTypes/clu";
 
 const DirectQuestion = (props: Array<Entity>) => {
-  let searchCode: Array<Entity>
-  let ans = ''
-  let searchKeyList: string[] = []
-  props.forEach(sk => {if(sk.category === "Search Key") searchKeyList.push(sk.extraInformation?.[0].key!)})
-  searchCode = props.filter(f => f.category !== "Search Key")
+  let ans = "";
+  const searchKeyList: string[] = [];
+
+  const searchCode: Array<Entity> = props.filter(
+    (f) => f.category !== "Search Key"
+  );
+
+  props.forEach((sk) => {
+    if (sk.category === "Search Key")
+      searchKeyList.push(sk.extraInformation?.[0].key!);
+  });
   if (!searchCode || !searchKeyList) return "No answer found!";
-  searchCode.forEach(sc => {
-    const searched = List.find(l => l[sc.category].replace(/ /g,'') === sc.text.toUpperCase().replace(/ /g,''))
-    if (!searched) return ans = "No answer found!";
-    searchKeyList.forEach(sk => {
-      ans += `${sk} of ${sc.text} is ${searched?.[sk]}\n`
-    })
-    return ans
-  })
-  return ans
+  searchCode.forEach((sc) => {
+    const searched = List.find(
+      (l) =>
+        l[sc.category].replace(/ /g, "") ===
+        sc.text.toUpperCase().replace(/ /g, "")
+    );
+    if (!searched) return "No answer found!";
+    searchKeyList.forEach((sk) => {
+      ans += `${sk} of ${sc.text} is ${searched?.[sk]}\n`;
+    });
+    return ans;
+  });
+  return ans;
 }
 
 export default DirectQuestion;

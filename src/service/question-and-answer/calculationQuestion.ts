d5d3462538e1d5a5cf1a0f229013762fdf559755
PhucHien21 <hiennguyen210901@gmail.com>
2022-12-12 14:38:52 +0200

    new calculation

--- src/service/question-and-answer/calculationQuestion.ts
+++ src/service/question-and-answer/calculationQuestion.ts
@@ -7,35 +7,32 @@ const CalculationQuestion = (props: Array<Entity>) => {
   let ans = ''
   let searchKeyList: string[] = []
   props.forEach(sk => {if(sk.category === "Search Key") searchKeyList.push(sk.extraInformation?.[0].key!)})
+
+  let searchValue: Array<Entity>
+  searchValue = props.filter(f => f.category === "Value")
+
   let searchedList: any[] = []
   let text: string[] = []
   props.forEach(sc => {if(sc.category !== "Search Key" && sc.category !== "Value") {
     searchedList.push(List.find(l => l[sc.category].replace(/ /g,'') === sc.text!.toUpperCase().replace(/ /g,'')))
     text.push(sc.text!)
   }})
-  let searchValue: Array<Entity>
-  searchValue = props.filter(f => f.category === "Value")
-  if (!searchedList || !searchKeyList || !searchValue) return ans = "No answer found!"
-  searchValue.forEach(sv => {
-    let m: number[] = []
-    if (sv.resolutions?.[0].resolutionKind === "LengthResolution") {
-      searchedList.forEach(sl => m.push(math.unit(sv.text).toNumber('m') * sl.Mass))
-    } else {for (let i = 0; i < searchedList.length; i++) m.push(math.unit(sv.text).toNumber('kg'))}
-    searchKeyList.forEach(sk => {
-      switch (sk) {
-        case 'Mass':
-          for (let i = 0; i < m.length; i++) {
-            ans += `${sk} of ${searchValue[i].text!} ${text[i]}: ${m[i].toFixed(3)}kg\n`
-          }
-          break;
-        case 'Cost':
-          for (let i = 0; i < m.length; i++) {
-            ans += `${sk} of ${searchValue[i].text!} ${text[i]}: ${(m[i]*searchedList[i].Cost).toFixed(3)}€\n`
-          }
-          break;
-      }
-    })
+
+  if (!searchedList || !searchKeyList || !searchValue) return "No answer found!"
+
+  searchKeyList.forEach(sk => {
+    for (let i = 0; i < searchedList.length; i++) {
+      searchValue.forEach(sv => {
+        let m: number = 0
+        if (sv.resolutions?.[0].resolutionKind === "LengthResolution") m = math.unit(sv.text).toNumber('m') * searchedList[i].Mass
+        else m = math.unit(sv.text).toNumber('kg')
+      
+        if (sk === 'Mass')  ans += `${sk} of ${sv.text!} ${text[i]}: ${m.toFixed(3)}kg\n`
+        else if (sk === 'Cost') ans += `${sk} of ${sv.text!} ${text[i]}: ${(m*searchedList[i].Cost).toFixed(3)}€\n`
+      })
+    }
   })
+
   return ans
 }
 
